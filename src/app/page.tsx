13ad6a239d4977e23f4031fb52819e57174130b8
Hussnain Raza <hussnain444all@gmail.com>
2024-09-04 11:25:15 +0500

    fixed content overflowing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,20 +11,25 @@ export default async function Page() {
   const cast = await new MoviesService().fetchMovieCast(movies[0].id);
 
   return (
-    <main>
+    <main style={{ margin: 0, padding: 0, boxSizing: 'border-box' }}>
       <head>
         <style>
-          @import
-          url('https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+          {`
+            @import url('https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+          `}
         </style>
       </head>
-      <HeroSection
-        title={movies[0].title}
-        backgroundImage={`https://image.tmdb.org/t/p/original/${movies[0]?.backdrop_path}`}
-        plot={movies[0].overview}
-        cast={cast}
-      />
-      <MoviesList movies={moviesWithImages} />
+      <div className="hero-section">
+        <HeroSection
+          title={movies[0].title}
+          backgroundImage={`https://image.tmdb.org/t/p/original/${movies[0]?.backdrop_path}`}
+          plot={movies[0].overview}
+          cast={cast}
+        />
+      </div>
+      <div className="movies-list">
+        <MoviesList movies={moviesWithImages} />
+      </div>
     </main>
   );
 }
