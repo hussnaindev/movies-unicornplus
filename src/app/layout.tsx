a92dc26c8918e2a78aa9d234664e15907bd334f1
Hussnain Raza <hussnain444all@gmail.com>
2024-09-05 08:16:46 +0500

    integrated google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
-import type { Metadata } from "next";
+import { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Head from "next/head"; // Import Head
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,11 +12,28 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
+      <Head>
+        {/* Google Tag Manager Script */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-BDX88Z8MCP"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-BDX88Z8MCP');
+            `,
+          }}
+        />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
